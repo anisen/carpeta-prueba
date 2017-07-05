872a78ee756d45e572097b50702a2e0c4fde720a
anisen <ananavelez@gmail.com>
2017-07-05 12:15:42 -0400

    denuevootrooo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,30 @@
 
-FB.getLoginStatus(function(response) {
-	statusChangeCallback(response);
-});
+
+ventana . fbAsyncInit = función () { 
+	FB . init ({ 
+		Id de aplicación       : '485536455130563' , 
+		galletas      : verdad , 
+		XFBML       : cierto , 
+		la versión     : 'v2.8' }); 
+	FB . AppEvents . logPageView (); };      
+
+
+
+	( Función ( d , s , ID ) { var js , FJS = d . GetElementsByTagName ( s ) [ 0 ]; si ( d . GetElementById ( ID )) { retorno ;} 
+		js = d . CreateElement ( s ); js . ID = Identificación ; 
+		js . src = "//connect.facebook.net/en_US/sdk.js" ; 
+		FJS . parentNode . insertBefore ( js , FJS ); } ( documento , 'guión' , 'facebook-jssdk' )); 
+	
+
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
 
 
 
 
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
